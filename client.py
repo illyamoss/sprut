d4dbfa8a145770716d0a81d2b7b4c6efd02fa511
qXytreXp <illya08mosiychuk@gmail.com>
2021-12-22 21:45:28 +0200

    Added multiple files transfer

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
+import logging
 import socket
 
+
+logging.basicConfig(level=logging.DEBUG)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("127.0.0.1", 8000))
@@ -7,17 +10,25 @@ sock.connect(("127.0.0.1", 8000))
 accept = input("Accept files?(Y/n): ").lower()
 
 if accept == "y":
-    sock.sendall("data accepted".encode())
+    sock.send("data accepted".encode())
 elif accept == "n":
+    sock.close()
     exit()
 else:
+    sock.close()
     print("Wrong answer")
     exit()
 
 while True:
-    data, addrs = sock.recvfrom(1024)
+    file = sock.recv(1024).decode()
+    if not file:
+        break
 
-    with open("file1.txt", "wb") as file:
-        print(data, data.decode())
-        file.write(data)
-        sock.close()
+    with open(file, "wb") as file:
+        data = sock.recv(1024)
+        while data != b"\x00":
+            file.write(data)
+            data = sock.recv(1024)
+
+sock.close()
+logging.info("Data successful transferred")
--- server.py
+++ server.py
@@ -1,21 +1,36 @@
+import sys
+import logging
 import socket
 
+logging.basicConfig(level=logging.DEBUG)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(("127.0.0.1", 8000))
 sock.listen(1)
 
-print("Server started")
-conn, addrs = sock.accept()
-print(addrs)
+logging.info("Server started")
 
-try:
-    while True:
-        data = conn.recv(1024)
-        print(data)
-        
-        if data == "data accepted":
-            with open("file.txt", "r") as file:
-                conn.send(file.read().encode())
-except KeyboardInterrupt:
-    sock.close()
+files = sys.argv[1:]
+while True:
+    conn, addrs = sock.accept()
+    logging.info(f"Connection: {addrs}")
+    data = conn.recv(1024)
+    logging.info(f"Data from client: {data}")
+    
+    if data.decode() == "data accepted":
+        for file in files:
+            logging.info("Sending files...")
+            conn.send(file.encode())
+
+            with open(file, "r") as file:
+                for line in file.readlines():
+                    conn.send(line.encode())
+            conn.send(b"\x00")
+
+        conn.close()
+        logging.info("Data succussful transferred")
+        break
+    else:
+        conn.close()
+        break
+sock.close()
