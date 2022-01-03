282b9edf314db1ca21e3d63cc406d859c0f401af
qXytreXp <illya08mosiychuk@gmail.com>
2022-01-03 14:56:08 +0200

    Decomposition

--- src/main.py
+++ src/main.py
@@ -1,87 +1,77 @@
-import os
-import sys
+# elif sys_args[0] == "recieve":
+#     sock.connect(("127.0.0.1", 8000))
+
+#     logging.info("Client connected")
+
+#     accept = input("Accept files?(Y/n): ").lower()
+
+#     match accept:
+#         case "y":
+#             sock.send("data accepted".encode())
+#         case _:
+#             sock.close()
+#             exit()
+
+#     while True:
+#         filename = sock.recv(1024).decode()
+#         if not filename:
+#             break
+#         recieve_files_from_server(server_socket=sock, filename=filename)
+
+#     logging.info("Data succussful transferred")
+
+
+# sock.close()
 import logging
-import socket
+import sys
+
+from tcp.tcp import (
+    sock, 
+    init_server,
+    connect_to_server, 
+    recieve_files_from_server,
+    send_file_to_client
+)
 
 
-logging.basicConfig(level=logging.DEBUG)
+if __name__ == "__main__":
+    sys_args = sys.argv[1:]
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    match sys_args[0].strip():
+        case "send":
+            server = init_server(host="127.0.0.1", port=8000)
+            client, addrs = server.accept()
+            logging.info(f"Connection: {addrs}")
 
-sys_args = sys.argv[1:]
+            files = sys_args[1:]  # Select input files name, except command "send"
 
-
-def send_file_to_client(
-    client_socket: socket.socket, path_to_file: str
-) -> None:
-    """ Send file from server to client """
-    client_socket.send(os.path.basename(path_to_file).encode())
-    with open(path_to_file, "r") as file:
-        for line in file.readlines():
-            client_socket.send(line.encode())
-    client_socket.send(b"\x00")  # End of the file
-
-
-def recieve_files_from_server(
-    server_socket: socket.socket, 
-    filename: str
-) -> None:
-    with open(filename, "wb") as file:
-        data = server_socket.recv(1024)
-        while data != b"\x00":
-            file.write(data)
-            data = server_socket.recv(1024)
-
-
-if sys_args[0] == "send":
-    files = sys_args[1:]
-
-    sock.bind(("127.0.0.1", 8000))
-    sock.listen(1)
-
-    logging.info("Server started")
-
-    while True:
-        client, addrs = sock.accept()
-        logging.info(f"Connection: {addrs}")
-        data = client.recv(1024)
-        logging.info(f"Data from client: {data}")
-        
-        if data.decode() == "data accepted":
-            for path_to_file in files:
+            if client.recv(1024).decode() == "data accepted":
                 logging.info("Sending files...")
-                send_file_to_client(
-                    client_socket=client, 
-                    path_to_file=path_to_file
-                )
+                for file_ in files:
+                    send_file_to_client(client=client, path_to_file=file_)
+                logging.info("Data succussful transferred")
 
             client.close()
+
+        case "recieve":
+            server = connect_to_server(host="127.0.0.1", port=8000)
+
+            accept = input("Accept files?(Y/n): ").lower()
+
+            if accept == "y":
+                server.send("data accepted".encode())
+            else:
+                server.close()
+                exit()
+
+            while True:
+                filename = server.recv(1024).decode()
+                if not filename:
+                    break
+                recieve_files_from_server(server=server, filename=filename)
+
             logging.info("Data succussful transferred")
-            break
-        else:
-            client.close()
-            break
-elif sys_args[0] == "recieve":
-    sock.connect(("127.0.0.1", 8000))
-
-    logging.info("Client connected")
-
-    accept = input("Accept files?(Y/n): ").lower()
-
-    match accept:
-        case "y":
-            sock.send("data accepted".encode())
+            server.close()
         case _:
+            print("Wrong Command")
             sock.close()
-            exit()
-
-    while True:
-        filename = sock.recv(1024).decode()
-        if not filename:
-            break
-        recieve_files_from_server(server_socket=sock, filename=filename)
-
-    logging.info("Data succussful transferred")
-
-
-sock.close()
--- src/tcp/tcp.py
+++ src/tcp/tcp.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import logging
 import socket
 
@@ -7,81 +6,76 @@ import socket
 logging.basicConfig(level=logging.DEBUG)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-sys_args = sys.argv[1:]
-
-
-def send_file_to_client(
-    client_socket: socket.socket, path_to_file: str
-) -> None:
-    """ Send file from server to client """
-    client_socket.send(os.path.basename(path_to_file).encode())
-    with open(path_to_file, "r") as file:
-        for line in file.readlines():
-            client_socket.send(line.encode())
-    client_socket.send(b"\x00")  # End of the file
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 
-def recieve_files_from_server(
-    server_socket: socket.socket, 
-    filename: str
-) -> None:
-    with open(filename, "wb") as file:
-        data = server_socket.recv(1024)
-        while data != b"\x00":
-            file.write(data)
-            data = server_socket.recv(1024)
-
-
-if sys_args[0] == "send":
-    files = sys_args[1:]
-
-    sock.bind(("127.0.0.1", 8000))
+def init_server(host: str, port: int) -> socket.socket:
+    sock.bind((host, port))
     sock.listen(1)
 
     logging.info("Server started")
-
-    while True:
-        client, addrs = sock.accept()
-        logging.info(f"Connection: {addrs}")
-        data = client.recv(1024)
-        logging.info(f"Data from client: {data}")
-        
-        if data.decode() == "data accepted":
-            for path_to_file in files:
-                logging.info("Sending files...")
-                send_file_to_client(
-                    client_socket=client, 
-                    path_to_file=path_to_file
-                )
-
-            client.close()
-            logging.info("Data succussful transferred")
-            break
-        else:
-            client.close()
-            break
-elif sys_args[0] == "recieve":
-    sock.connect(("127.0.0.1", 8000))
-
-    logging.info("Client connected")
-
-    accept = input("Accept files?(Y/n): ").lower()
-
-    match accept:
-        case "y":
-            sock.send("data accepted".encode())
-        case _:
-            sock.close()
-            exit()
-
-    while True:
-        filename = sock.recv(1024).decode()
-        if not filename:
-            break
-        recieve_files_from_server(server_socket=sock, filename=filename)
-
-    logging.info("Data succussful transferred")
+    return sock
 
 
-sock.close()
+def connect_to_server(host: str, port: int) -> socket.socket:
+    sock.connect((host, port))
+
+    logging.info("Client successful connected to server")
+    return sock
+
+
+def send_file_to_client(
+    client: socket.socket, path_to_file: str
+) -> None:
+    """ Send file from server to client """
+    client.send(os.path.basename(path_to_file).encode())
+    with open(path_to_file, "r") as file:
+        for line in file.readlines():
+            client.send(line.encode())
+    client.send(b"\x00")  # End of a file
+
+
+def recieve_files_from_server(
+    server: socket.socket, 
+    filename: str
+) -> None:
+    with open(filename, "wb") as file:
+        data = server.recv(1024)
+        while data != b"\x00":
+            file.write(data)
+            data = server.recv(1024)
+
+
+# def data_from_client(
+#     client: socket.socket
+# ) -> tuple[socket.socket, bytes]:
+#     while True:
+#         data = client.recv(1024)
+#         logging.info(f"Data from client: {data}")
+
+#         if not data:
+#             server.close()
+#             break
+
+#         yield client, data
+
+
+def send_file(
+    server: socket.socket, 
+    client: socket.socket, 
+    data_from_client: bytes, 
+    files: list
+) -> None:
+    if data_from_client.decode() == "data accepted":
+        for path_to_file in files:
+            logging.info("Sending files...")
+            send_file_to_client(
+                client_socket=client, 
+                path_to_file=path_to_file
+            )
+
+        client.close()
+        logging.info("Data succussful transferred")
+    else:
+        client.close()
+    server.close()
